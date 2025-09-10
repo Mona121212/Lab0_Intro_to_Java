ba90c8f3b7b475cf80e4f05df3c08a85fd8ed174
Yusif Najafli <ynajafli@outlook.com>
2025-09-10 16:06:11 -0600

    driver app

--- src/app/AppDriver.java
+++ src/app/AppDriver.java
@@ -1,5 +1,9 @@
 package app;
+import model.*;
 
 public class AppDriver {
-
+	public static void main(String[] args) {
+		Refrigerator test = new Refrigerator("1", "test", 5, 10, "Red", 12.50, 2, 10, 15);
+		System.out.println(test);
+	}
 }
--- src/model/Appliance.java
+++ src/model/Appliance.java
@@ -1,5 +1,74 @@
 package model;
 
 public abstract class Appliance {
+	private String itemNumber;    
+    private String brand;         
+    private int quantity;         
+    private int wattage;          
+    private String color;         
+    private double price;         
+    
+   
+    public Appliance(String itemNumber, String brand, int quantity, 
+                    int wattage, String color, double price) {
+        this.itemNumber = itemNumber;
+        this.brand = brand;
+        this.quantity = quantity;
+        this.wattage = wattage;
+        this.color = color;
+        this.price = price;
+    }
+    
+    
+    public String getItemNumber() {
+        return itemNumber;
+    }
+    
+    public String getBrand() {
+        return brand;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public int getWattage() {
+        return wattage;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    // Setter methods (only for attributes that need to be updated, e.g., quantity)
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+   
+    public boolean isAvailable() {
+        return quantity > 0;
+    }
+    
+
+    public void checkout() {
+        if (quantity > 0) {
+            quantity--;
+        }
+    }
+    
+ 
+    public String toString() {
+        return "ItemNumber: " + itemNumber + "\n" +
+               "Brand: " + brand + "\n" +
+               "Quantity: " + quantity + "\n" +
+               "Wattage: " + wattage + "\n" +
+               "Color: " + color + "\n" +
+               "Price: " + price;
+    }
 	
 }
