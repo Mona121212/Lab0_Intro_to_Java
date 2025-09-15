afd166908c8059a8dbb05bbe9a69f091a2a4fdcf
Mona <xiaomei.mona.he@gmail.com>
2025-09-14 21:19:52 -0600

    feat: dishwasher microwave and vacuum class

--- src/model/Dishwasher.java
+++ src/model/Dishwasher.java
@@ -1,5 +1,41 @@
 package model;
 
-public class Dishwasher {
+public class Dishwasher extends Appliance {
+	private String feature;   // special feature
+	private String soundRating;  // the level of sound
+
+	public Dishwasher(String itemNumber, String brand, int quantity, int wattage, String color, double price,
+			String feature, String soundRating) {
+		super(itemNumber, brand, quantity, wattage, color, price);
+		this.feature = feature;
+		this.soundRating = soundRating;
+		
+	}
+	
+	public String getFeature() {
+		return feature;
+	}
+	
+	public String getSoundRating() {
+		return soundRating;
+	}
+	
+	public String getSoundRatingDescription() {
+		switch(soundRating) {
+		case "Qt": return "Quietest";
+		case "Qr": return "Quieter";
+		case "Qu": return "Quiet";
+		case "M": return "Moderate";
+		default: return "Unknown";
+		
+		
+		}
+	}
+	
+	public String toString() {
+		return super.toString() + "\n" + 
+				"Feature: " + feature + "\n" +
+				"SoundRating: " + getSoundRatingDescription();
+	}
 
 }
--- src/model/Vacuum.java
+++ src/model/Vacuum.java
@@ -1,5 +1,38 @@
 package model;
 
-public class Vacuum {
+public class Vacuum extends Appliance{
+	
+	
+	private String grade;
+	private int batteryVoltage;
+	public Vacuum(String itemNumber, String brand, int quantity, int wattage, String color, double price,
+			String grade, int batteryVoltage) {
+			super(itemNumber, brand, quantity, wattage, color, price);
+			
+			this.grade = grade;
+			this.batteryVoltage = batteryVoltage;
+			
+		}
+	
+	public String getGrade() {
+		return grade;
+	}
+	
+	public int getBatteryVoltage() {
+		return batteryVoltage;
+	}
+	
+	private String getBatteryVoltageType() {
+		return (batteryVoltage == 18) ? "Low" : "High";
+	}
+	
+	public String toString() {
+		return super.toString() + "\n" +
+				"Grade: " + grade + "\n" +
+				"Battery voltage: " + getBatteryVoltageType();
+	}
 
+	
 }
+
+
