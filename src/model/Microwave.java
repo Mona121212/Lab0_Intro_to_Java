cd0b0a499218c75dbe239664cad708d48ed6ea91
Mona <xiaomei.mona.he@gmail.com>
2025-09-14 21:20:16 -0600

    feat: microwave class

--- src/model/Microwave.java
+++ src/model/Microwave.java
@@ -1,5 +1,34 @@
 package model;
 
-public class Microwave {
-
+public class Microwave extends Appliance{
+	
+	
+	private double capacity;
+	// char only use single character
+	private char roomType;
+	public Microwave(String itemNumber, String brand, int quantity, int wattage, String color, double price,
+			double capacity, char roomType) {
+			super(itemNumber, brand, quantity, wattage, color, price);
+			this.capacity = capacity;
+			this.roomType = roomType;
+			
+		}
+	
+	public double getCapacity() {
+		return capacity;
+	}
+	
+	public char getRoomType() {
+		return roomType;
+	}
+	
+	public String getRoomTypeDescription() {
+		return (roomType == 'K')? "Kitchen" : "Work Site";
+	}
+	
+	public String toString() {
+		return super.toString() + "\n" +
+				"Capacity: " + capacity + "\n" +
+				"Room Type: " + getRoomTypeDescription();
+	}
 }
