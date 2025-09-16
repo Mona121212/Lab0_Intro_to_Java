7f3cc8f1e7922b6da7b1ba75ed7c69e1934d5747
Mona <xiaomei.mona.he@gmail.com>
2025-09-15 23:55:48 -0600

    feat: main

--- src/app/AppDriver.java
+++ src/app/AppDriver.java
@@ -1,6 +1,49 @@
 package app;
 
+import java.io.IOException;
+import java.util.*;
+import model.*;
+import util.FileManager;;
+
+
 public class AppDriver {
+	 private List<Appliance> appliances;
+	 private Scanner scanner;
+	 private FileManager fileManager;
+	 
+	 public AppDriver() {
+		 appliances = new ArrayList<> ();
+		 scanner = new Scanner(System.in);
+		 fileManager = new FileManager();
+	 }
+	 
+	 /**
+	  * Main entry point of the application
+	  */
+	 
+	 public static void main(String[] args) {
+		 AppDriver app = new AppDriver();
+		 app.loadAppliances();
+		 app.displayMenu();
+	 }
 	
+	 private void loadAppliances() {
+		 try {
+			 appliances = fileManager.loadAppliances("appliances.txt");
+			 System.out.println("Appliances loaded successfully");
+		 } catch(IOException e) {
+			 System.out.println("Erro loading appliances:" + e.getMessage());
+			 appliances = new ArrayList<>();
+		 }
+		 
+	 }
+	 
+	 private void displayMenu() {
+		 int choice;
+		 do {
+			 System.out.println("\nWelcome to Modern Appliances!");
+			 System
+		 }
+	 }
 
 }
